17eb0c422f548ea2e946aad723ffedbf64646961
grannysbuds <b.kruchinkin@yandex.ru>
2024-05-14 17:27:23 +0300

    upd

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,3 @@
 window.addEventListener('load', () => {
-    document.getElementById('loadResult').innerHTML = "loaded";
+    document.getElementById('loadResult').innerHTML = window.Telegram.WebApp.initData;
 });
