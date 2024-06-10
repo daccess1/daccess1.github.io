194132309e69048adb5a46a09df053b4adf3f6f4
grannysbuds <b.kruchinkin@yandex.ru>
2024-06-10 16:33:12 +0300

    test friends

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
 let _wa;
+let _tg_user;
+let _player;
 
 
 window.addEventListener('load', () => {
     _wa = window.Telegram.WebApp;
-    _wa.expand();
-
-    console.log(_wa.initData);
-    console.log(getUserData());
+    getUserData().then(data => {
+        _wa.expand();
+    });
 
     _wa.BackButton.onClick(() => {
         navigations.pop();
@@ -29,10 +30,19 @@ function inviteButtonClick() {
 function copyUrlButtonClick() {
     navigator.clipboard.writeText("Umperium bot share url");
 }
+``
+async function getUserData() {
+    let userPayload = decodeURIComponent(_wa.initData);
+    userPayload = userPayload.substring(userPayload.indexOf('user=') + 5, userPayload.lastIndexOf('}') + 1);
 
-function getUserData() {
-    const decoded = decodeURI(_wa.initData);
-    return decoded;
+    if (userPayload) {
+        _tg_user = JSON.parse(userPayload);
+    }
+
+    if (_tg_user) {
+        const playerPayload = await fetch(`https://bba7p9tu9njf9teo8qkf.containers.yandexcloud.net/player/${_tg_user.id}`);
+        _player = await playerPayload.json();
+    }
 }
 
 function navigate(page) {
--- scripts/navigate.js
+++ scripts/navigate.js
@@ -17,35 +17,15 @@ function backendAPIRequest(url) {
 
 async function loadFriendsPage() {
     console.log('Loading friends page');
-    const viewF = await fetch("https://bba7p9tu9njf9teo8qkf.containers.yandexcloud.net/player/friends/018ff258-3ad0-7198-a240-e4499593faf3");
-    const viewFBody = await viewF.json();
-
-    console.log('XHR:', JSON.parse(view));
-    console.log('Fetch:', viewFBody);
+    const viewDataPayload = await fetch(`https://bba7p9tu9njf9teo8qkf.containers.yandexcloud.net/player/friends/${_player.ref_id}`);
+    const viewData = await viewDataPayload.json();
 
     const view = {
-        friends_count: 2,
-        friends: [
-            {
-                "user_id": 47,
-                "user_name": "Tony Montana",
-                "score": 0
-            },
-            {
-                "user_id": 48,
-                "user_name": "Антон Городецкий",
-                "score": 0
-            }
-        ]
+        friends_count: viewData.length,
+        friends: viewData
     }
 
-    const request = new XMLHttpRequest();
-    request.open("GET", "/pages/friends/page.template.html?v=1", true);
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
-            const template = request.responseText;
-            document.getElementById('pageContent').innerHTML = Mustache.render(template, view);
-        }
-    }
-    request.send();
+    const templateRequest = await fetch("/pages/friends/page.template.html?v=2");
+    const template = await templateRequest.text();
+    document.getElementById('pageContent').innerHTML = Mustache.render(template, view);
 }
