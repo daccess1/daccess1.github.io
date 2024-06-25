d8f2457fa4c30d81d1e66e5a26f510fecc0915b1
grannysbuds <b.kruchinkin@yandex.ru>
2024-06-25 18:55:12 +0300

    tap game

--- scripts/home.js
+++ scripts/home.js
@@ -8,14 +8,34 @@ document.addEventListener('loadHome', () => {
     const container = document.getElementById('tapGame');
     ['mousedown', 'touchstart'].forEach(eventType => {
         target.addEventListener(eventType, (event) => {
-            console.log(`x: ${event.pageX} y: ${event.pageY}`);
             container.classList.add('tapGame--tapped');
+            drawTapResult(event.pageX, event.pageY);
         });
     });
     ['mouseup', 'touchend', 'mouseleave'].forEach(eventType => {
-        target.addEventListener(eventType, (event) => {
-            console.log(`x: ${event.pageX} y: ${event.pageY}`);
+        target.addEventListener(eventType, () => {
             container.classList.remove('tapGame--tapped');
         });
     });
 });
+
+function drawTapResult(x, y) {
+    var el = document.createElement('div');
+    el.innerHTML = '+' + _player.tap_increment;
+    el.classList.add('tapResult');
+    el.style.left = `${x - 20}px`;
+    el.style.top = `${y - 20}px`;
+    document.body.appendChild(el);
+
+    el.animate([
+        { transform:'translateY(0px)', opacity: 1 },
+        { transform:'translateY(-150px)', opacity: 0 },
+    ], {
+        duration: 500,
+        iterations: 1,
+    });
+
+    setTimeout(() => {
+        el.remove();
+    }, 500);
+}
