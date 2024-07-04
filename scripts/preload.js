50cf839088a2045fc53001a1a37a51f8e332912b
grannysbuds <b.kruchinkin@yandex.ru>
2024-07-04 14:05:29 +0300

    increase player size

--- scripts/preload.js
+++ scripts/preload.js
@@ -35,7 +35,7 @@ document.addEventListener('preload', async () => {
 });
 
 async function preload() {
-    const ver = 59;
+    const ver = 60;
 
     const scripts = [
         `/scripts/index.js?v=${ver}`,
